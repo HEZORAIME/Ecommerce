9d811f524454b8a0ec30cebbc6f723ae74ebe161
HEZORAIME <chanrojano@gmail.com>
2025-07-06 14:08:00 +0800

    Adding Login with Validation

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -40,3 +40,36 @@ export const registerUser = async (req, res) => {
     });
   }
 };
+
+export const LoginUser = async (req, res) => {
+  // extract user credentials
+  const { email, password } = req.body;
+  try {
+    // find user in database
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // compare the password hash that are in database(mongoodb)
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(40).json({ message: "Invalid email or password" });
+    }
+
+    // if everything is correct let say the password is correct and
+    //  email also it will return success message and user can login
+    return res.status(200).json({
+      message: "Login Successful",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.Role
+      },
+    });
+  } catch (error) {
+    console.error("Error during login", error);
+    return res.status(500).json({message: "Internal Error!"});
+  }
+};
