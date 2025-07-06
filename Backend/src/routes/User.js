86a438ebe2e167de7e2e91d789d985292d4ab7af
HEZORAIME <chanrojano@gmail.com>
2025-07-06 13:27:41 +0800

    Update

--- Backend/src/routes/User.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { registerUser } from '../controllers/userController';
-
-const router = express.Router();
-
-// Route to register a new user
-router.post('/register', registerUser)
