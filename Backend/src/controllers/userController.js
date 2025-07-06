da961e6a93b4b43537d42606bdaa0d586ea8f74e
HEZORAIME <chanrojano@gmail.com>
2025-07-06 14:01:49 +0800

    refactor(userController): clean up commented code and update imports

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -1,38 +1,5 @@
-// import User from '../models/User.js';
-
-// // to hash passwords
-// import bcrypt from 'bcrypt';
-
-
-// export const registerUser = async (req, res) => {
-//     const {name, email, password, Role} = req.body;
-//     try {
-//         const existingUser = await User.findOne({ email });
-//         if (existingUser.length > 0) {
-//             return res.status(400).json({message: "User Already Exists!"})
-//         }
-//         const hashedPassword = await bcrypt.hash(password, 10);
-//         const user = await User.create({
-//             name,
-//             email,
-//             password: hashedPassword,
-//             Role: Role || 'user' // default role is user
-//         })
-//         res.status(201).json({
-//             message: 'User Registered Successfully!'
-//         })
-//     } catch (error) {
-//         console.error("error in registering user", error);
-//         res.status(500).json({
-//             message: "Internal Server Error",
-//             error: error.message
-//         })
-//     }
-// }
-
-
-import User from '../models/User.js';
-import bcrypt from 'bcrypt';
+import User from "../models/User.js";
+import bcrypt, { compare } from "bcrypt";
 /**
  * Registers a new user.
  * - Validates email uniqueness
