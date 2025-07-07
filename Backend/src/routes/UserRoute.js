471cef120c3bff4100ecbbb6a109656f98dd84a0
HEZORAIME <chanrojano@gmail.com>
2025-07-07 17:16:55 +0800

    Adding route for Login

--- Backend/src/routes/UserRoute.js
+++ Backend/src/routes/UserRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { registerUser } from '../controllers/userController.js';
+import { registerUser, LoginUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
 // Route to register a new user
 router.post('/register', registerUser)
+router.post('/Login', LoginUser)
 
 export default router;
