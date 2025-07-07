398059b9b036295e762998a6e3aa86a6dfe331e1
HEZORAIME <chanrojano@gmail.com>
2025-07-08 07:17:50 +0800

    add authentication middleware with token verification

---
+++ Backend/src/middleware/auth.js
@@ -0,0 +1,38 @@
+import {verifyToken} from '../utils/jwt.js';
+import User from '../models/User.js';
+
+
+export const authenticateToken = async (req, res, next) => {
+    try {
+        // Extract token from cookies to be used for authentication
+        const token = req.cookies.token;
+        if (!token) {
+            return res.status(401).json({message: "Acces denied. No token provided."});
+        }
+        // verify the token so that the user can access the protected routes
+        const decoded = verifyToken(token);
+        if (!decoded) {
+            return res.status(401).json({message: "Invalid Token"});
+        }
+        // find and get the user from the database to check if the user is valid or not
+        const user = await User.findById(decoded.userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found." });
+        }
+        // if the user is valid then we can access the protected routes
+        req.user = user;
+        next();
+    } catch (error) {
+        console.error("error during authentication", error);
+        return res.status(500).json({ message: "Invalid token!" });
+    }
+};
+
+
+
+export const isAdmin = (req, res, next) => {
+    if (req.user.Role !=='admin') {
+        return res.status(403).json({message: "Access denied."});
+    }
+    next();
+};
