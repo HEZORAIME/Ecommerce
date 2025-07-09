979498c54d4c0be20b92ea048d35c43e6833a668
HEZORAIME <chanrojano@gmail.com>
2025-07-09 10:49:36 +0800

    refactor to organize the code

--- Backend/src/routes/UserRoute.js
+++ Backend/src/routes/UserRoute.js
@@ -1,13 +1,24 @@
-import express from 'express';
-import { registerUser, LoginUser, LogoutUser, GetUserProfile } from '../controllers/userController.js';
-import { authenticateToken } from '../middleware/auth.js';
+import express from "express";
+import {
+  registerUser,
+  LoginUser,
+  LogoutUser,
+  GetUserProfile,
+} from "../controllers/userController.js";
+import { authenticateToken, isAdmin } from "../middleware/auth.js";
 
 const router = express.Router();
 
 // Routes
-router.post('/register', registerUser)
-router.post('/login', LoginUser)
-router.post('/logout', authenticateToken, LogoutUser)
-router.get('/profile',authenticateToken, GetUserProfile)
+router.post("/register", registerUser);
+router.post("/login", LoginUser);
+router.post("/logout", authenticateToken, LogoutUser);
+router.get("/profile", authenticateToken, GetUserProfile);
+
+
+
+router.get("/admin/dashboard", authenticateToken, isAdmin, (_req, res) => {
+    res.status(200).json({message: "Admin dashboard access successful"});
+});
 
 export default router;
