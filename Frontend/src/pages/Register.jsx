d51b9c9fe6ccfb4a9130ff799e2532c320ba4c07
HEZORAIME <chanrojano@gmail.com>
2025-07-14 19:55:58 +0800

    feat: Update for User registration

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -10,10 +10,36 @@ export default function Register() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await api.post('/users/register', {username, password, email, Role});
-            
-        } catch (error) 
-
-
+            const response = await api.post('/users/register', {username, password, email});
+            if (response.status === 200) {
+                window.location.href = '/login';
+            } else {
+                setError('Registration failed',error);
+            }
+        } catch (error) {
+            console.error("Error registering user", error)
+            return error.status(500).json({massage: "Internal server error"});
+        }
+        if (!username || !password || !email) {
+            setError("Please fill all the");
+            return;
+        }
     }
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="">
+                Username:
+                <input  placeholder="Username" type="username" value={username} onChange={e => setUsername(e.target.value)}/>
+            </label>
+            <label htmlFor="">
+                Email:
+                <input  placeholder="Please Enter email" type="email" value={email} onChange={e => setEmail(e.target.value)}/>
+            </label>
+            <label>
+                Password:
+                <input type="password" placeholder="Enter your password" value={password} onChange={e =>setPassword(e.target.value)}/>
+            </label>
+            <button type="submit">Register</button>
+        </form>
+    )
 }
