8761a17f89a981a6c5cab7b7166784abaa5b89b6
HEZORAIME <chanrojano@gmail.com>
2025-07-14 19:56:42 +0800

    feat:  route for register page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router';
 import Login from './pages/Login';
+import Register from './pages/Register';
 import UserDashboard from './pages/userDashboard';
 import AdminDashboard from './pages/adminDashboard';
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/Register" element={<Register />} />
       <Route path="/dashboard" element={<UserDashboard />} />
       <Route path="/admin/dashboard" element={<AdminDashboard />} />
     </Routes>
