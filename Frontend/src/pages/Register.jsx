06cbc254fcb3b753cf5e9f16319939a80bb42995
HEZORAIME <chanrojano@gmail.com>
2025-07-16 21:56:57 +0800

    feat(Register) Fix the error

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -1,50 +1,76 @@
 import { useState } from "react";
-import api from '../utils/api';
-import {Link} from "react-router"
+import api from "../utils/api";
+import { Link } from "react-router";
 
 export default function Register() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [email, setEmail] = useState('');
-    const [error, setError] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await api.post('/users/register', {username, password, email});
-            if (response.status === 200) {
-                window.location.href = '/login';
-            } else {
-                setError('Registration failed',error);
-            }
-        } catch (error) {
-            console.error("Error registering user", error)
-            return error.status(500).json({massage: "Internal server error"});
-        }
-        if (!username || !password || !email) {
-            setError("Please fill all the");
-            return;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (!username || !password || !email) {
+        setError("Please fill all the");
+        return;
+      }
+      const response = await api.post("/users/register", {
+        username,
+        password,
+        email,
+        name: username,
+      });
+      if (response.status === 200 || response.status === 201) {
+        window.location.href = "/login";
+      } else {
+        setError("Registration failed");
+      }
+      console.log("Registration response status:", response.status);
+
+    } catch (error) {
+        if (error.response?.status === 409) {
+            setError("Username or email already in use please use another one")
+        } else {
+            console.error("error registering user", error);
+            setError("internal server error");
         }
     }
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="">
-                Username:
-                <input  placeholder="Username" type="username" value={username} onChange={e => setUsername(e.target.value)}/>
-            </label>
-            <label htmlFor="">
-                Email:
-                <input  placeholder="Please Enter email" type="email" value={email} onChange={e => setEmail(e.target.value)}/>
-            </label>
-            <label>
-                Password:
-                <input type="password" placeholder="Enter your password" value={password} onChange={e =>setPassword(e.target.value)}/>
-            </label>
-            <button type="submit">Register</button>
-            <p>
-                Already have account? <Link to = "/login">Login</Link>
-            </p>
-            {error && <p>{error}</p>}
-        </form>
-    )
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="">
+        Username:
+        <input
+          placeholder="Username"
+          type="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label htmlFor="">
+        Email:
+        <input
+          placeholder="Please Enter email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button type="submit">Register</button>
+      <p>
+        Already have account? <Link to="/login">Login</Link>
+      </p>
+      {error && <p>{error}</p>}
+    </form>
+  );
 }
