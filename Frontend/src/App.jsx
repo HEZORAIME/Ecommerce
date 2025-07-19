c360b82f905b973229a8d367ccd110e91d7869e8
HEZORAIME <chanrojano@gmail.com>
2025-07-19 12:35:09 +0800

    feat(App): Adding Protected route(User/Admin)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,31 @@
-import { Routes, Route } from 'react-router';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import UserDashboard from './pages/userDashboard';
-import AdminDashboard from './pages/adminDashboard';
+import { Routes, Route } from "react-router";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import UserDashboard from "./pages/userDashboard";
+import AdminDashboard from "./pages/adminDashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<UserDashboard />} />
-      <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <UserDashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/admin/dashboard"
+        element={
+          <ProtectedRoute>
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
