4e31fb1265449c3dd2561aac72bdccbb15aba53c
HEZORAIME <chanrojano@gmail.com>
2025-07-20 18:13:16 +0800

    feat(productController): Logic for product

---
+++ Backend/src/controllers/productController.js
@@ -0,0 +1,22 @@
+import Product from "../models/Product.js";
+
+export const createProduct = async (req, res) => {
+    const {name, price, description, images,
+        category, stock, sold, ratings, numOfreviews, reviews} = req.body;
+        try {
+            if (!name || !price || !description || !images || !category || !stock) {
+                return res.status(400).json({message: "All fields are required"})
+            } else {
+                const product = new Product({Role: "admin", 
+                    name, price, description, images, category, stock,
+                    sold, ratings, numOfreviews, reviews
+                });
+                await product.save();
+                res.status(201).json({message: "Product created successfully"});
+
+            }
+        } catch (err) {
+            res.status(500).json({message: "Internal server error"});
+            console.error("Error creating product", err);
+        }
+}
