654ac26dc177362f0e32d97c2c8f1fd373244660
HEZORAIME <chanrojano@gmail.com>
2025-07-20 18:58:19 +0800

    feat(refactor): remove the hardcoded code admin

--- Backend/src/controllers/productController.js
+++ Backend/src/controllers/productController.js
@@ -1,22 +1,22 @@
 import Product from "../models/Product.js";
 
 export const createProduct = async (req, res) => {
-    const {name, price, description, images,
-        category, stock, sold, ratings, numOfreviews, reviews} = req.body;
-        try {
-            if (!name || !price || !description || !images || !category || !stock) {
-                return res.status(400).json({message: "All fields are required"})
-            } else {
-                const product = new Product({Role: "admin", 
-                    name, price, description, images, category, stock,
-                    sold, ratings, numOfreviews, reviews
-                });
-                await product.save();
-                res.status(201).json({message: "Product created successfully"});
+    const { name, price, description, images,
+        category, stock, sold, ratings, numOfreviews, reviews } = req.body;
+    if (!name || !price || !description || !images || !category || !stock) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+    try {
+        const product = new Product({
+            name, price, description, images, category, stock,
+            sold, ratings, numOfreviews, reviews
+        });
+        await product.save();
+        res.status(201).json({ message: "Product created successfully" });
 
-            }
-        } catch (err) {
-            res.status(500).json({message: "Internal server error"});
-            console.error("Error creating product", err);
-        }
+
+    } catch (err) {
+        res.status(500).json({ message: "Internal server error" });
+        console.error("Error creating product", err);
+    }
 }
