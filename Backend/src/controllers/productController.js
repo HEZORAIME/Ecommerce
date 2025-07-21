7a9a1ea0dc6857851a0aa0c5127f076b051707f6
HEZORAIME <chanrojano@gmail.com>
2025-07-21 10:02:33 +0800

    feat(product): add product review functionality

--- Backend/src/controllers/productController.js
+++ Backend/src/controllers/productController.js
@@ -20,3 +20,28 @@ export const createProduct = async (req, res) => {
         console.error("Error creating product", err);
     }
 }
+export const addProductReview = async(req, res) => {
+    const productId = req.params.productId;
+    const {user, name, comment, rating} = req.body;
+    if(!user|| !name|| !comment || !rating == null) {
+        return res.status(400).json({message: "All fields are required"});
+    }
+
+    try {
+        const product = await Product.findById(productId);
+        if (!product) {
+            return res.status(404).json({message: "Product not found"});
+        }
+
+        const newReview = {user, name, comment, rating};
+        product.reviews.push(newReview);
+        product.numOfreviews = product.reviews.length;
+        product.ratings = product.reviews.reduce((acc, item) => acc + item.rating, 0) / product.numOfreviews;
+
+        await product.save();
+        res.status(201).json({message: "Review added successfully", product});
+    } catch(err) {
+        res.status(500).json({message: "Internal server error"});
+        console.log("Error adding review", err);
+    }
+}
