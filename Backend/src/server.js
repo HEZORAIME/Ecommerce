45e1d9fc443fad1b537a67cb00f9c1d8c7f6e5fb
HEZORAIME <chanrojano@gmail.com>
2025-07-22 20:59:52 +0800

    feat(server): add product route to express app

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,9 +3,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import UserRoute from './routes/UserRoute.js'
 import cookieParser from 'cookie-parser';
+import ProductRoute from './routes/ProductRoute.js';
 
 dotenv.config();
-
 
 
 const app = express();
@@ -18,6 +18,7 @@ app.use(cookieParser());
 
 // api routes
 app.use("/api/users", UserRoute);
+app.use("/api/products", ProductRoute);
 
 connectDB().then(() => {
     app.listen(PORT, () => {
