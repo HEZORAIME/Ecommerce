16c94c6da9989d7d4f073b2191b431c0aa2f5cb7
HEZORAIME <chanrojano@gmail.com>
2025-07-23 08:00:30 +0800

    fix(jwt): update token generation to include user role for enhanced security

--- Backend/src/utils/jwt.js
+++ Backend/src/utils/jwt.js
@@ -1,8 +1,8 @@
 import jwt from 'jsonwebtoken';
 
-export const generateToken = (userId) => {
+export const generateToken = (userId, role) => {
     return jwt.sign(
-        { userId},
+        { userId, role},
         process.env.JWT_SECRET,
         { expiresIn: '1h'}
     );
