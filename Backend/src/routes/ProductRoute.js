766e9289eb56366cfddfc4c1331c560da721b25b
HEZORAIME <chanrojano@gmail.com>
2025-07-23 23:46:30 +0800

    fix(ProductRoute): update delete route to use isAdmin middleware and rename param

--- Backend/src/routes/ProductRoute.js
+++ Backend/src/routes/ProductRoute.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { addProductReview, getallProduct, getOneProduct, deleteOneProduct } from '../controllers/productController.js';
-import { authenticateToken } from "../middleware/auth.js";
+import { authenticateToken, isAdmin } from "../middleware/auth.js";
 const router = express.Router();
 
 // Routes for product
 router.post("/products/:productId/reviews", authenticateToken, addProductReview);
 router.get("/allproduct", authenticateToken, getallProduct);
 router.get("/product/:productId", authenticateToken, getOneProduct);
-router.delete("/product/:productId", authenticateToken, deleteOneProduct);
+router.delete("/product/:deleteProductId", authenticateToken, isAdmin, deleteOneProduct);
 
 export default router;
