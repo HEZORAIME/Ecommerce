8249ff511be45726841c203167d17b922c6daa74
HEZORAIME <chanrojano@gmail.com>
2025-07-26 20:14:11 +0800

    fix(server): remove the global ratelimiting that cause main problem to admin

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,7 +4,6 @@ import dotenv from 'dotenv';
 import UserRoute from './routes/UserRoute.js'
 import cookieParser from 'cookie-parser';
 import ProductRoute from './routes/ProductRoute.js';
-import {productRateLimiter} from './middleware/productRatelimiter.js';
 
 dotenv.config();
 
@@ -14,7 +13,6 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/products", productRateLimiter);
 
 
 // api routes
