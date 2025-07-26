ab9bcba3128cc1748ae003bf162f1b0027009b8a
HEZORAIME <chanrojano@gmail.com>
2025-07-26 20:12:59 +0800

    refactor(ProductRoute): adjust authentication and the ratelimiting will only work on users not on admin

--- Backend/src/routes/ProductRoute.js
+++ Backend/src/routes/ProductRoute.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import { addProductReview, getallProduct, getOneProduct, deleteOneProduct, updateProduct } from '../controllers/productController.js';
-import { authenticateToken, isAdmin } from "../middleware/auth.js";
+import { authenticateToken, isAdmin, isUser } from "../middleware/auth.js";
 import { productRateLimiter } from "../middleware/productRatelimiter.js";
 const router = express.Router();
 
-// Routes for product - authentication first, then rate limiting (skipped for admins)
-router.post("/products/:productId/reviews", authenticateToken, productRateLimiter, addProductReview);
-router.get("/allproduct", authenticateToken, productRateLimiter, getallProduct);
-router.get("/product/:productId", authenticateToken, productRateLimiter, getOneProduct);
-// Admin-only routes - no rate limiting needed since admins are skipped anyway
+// Routes for product - apply rate limiting only for users
+router.post("/products/:productId/reviews", authenticateToken, isUser, productRateLimiter, addProductReview);
+router.get("/allproduct", productRateLimiter, authenticateToken, isUser, getallProduct); // accessible by all authenticated users
+router.get("/allproductt", authenticateToken, isAdmin, getallProduct); // accessible by all authenticated users
+router.get("/product/:productId", authenticateToken, isUser, productRateLimiter, getOneProduct);
+// Admin-only routes - no rate limiting
 router.delete("/product/:deleteProductId", authenticateToken, isAdmin, deleteOneProduct);
 router.put("/product/:productId", authenticateToken, isAdmin, updateProduct);
 export default router;
