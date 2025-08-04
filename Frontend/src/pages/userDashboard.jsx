d270daf7b3959fca30342c4675dacb358960737f
HEZORAIME <chanrojano@gmail.com>
2025-08-04 22:55:46 +0800

    feat: add UserProducts component to user dashboard and update dependencies

--- Frontend/src/pages/userDashboard.jsx
+++ Frontend/src/pages/userDashboard.jsx
@@ -1,10 +1,12 @@
 import Navbar from '../components/Navbar'
+import UserProducts from '../components/UserProduct'
 
 export default function userDashboard() {
   return (
     <div>
      <Navbar/>
      <h1>User dashboard</h1>
+     <UserProducts/>
     </div>
   )
 }
