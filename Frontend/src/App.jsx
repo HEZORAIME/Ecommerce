b3216cc4a1fad2e18718787a51b237ab464e2e59
HEZORAIME <chanrojano@gmail.com>
2025-08-05 22:57:31 +0800

    refactor: standardize dashboard file naming convention

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import UserDashboard from "./pages/userDashboard";
-import AdminDashboard from "./pages/adminDashboard";
+import UserDashboard from "./pages/userdashboard";
+import AdminDashboard from "./pages/admindashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return (
+    
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
