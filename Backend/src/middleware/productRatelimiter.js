d5578d24e3d5baefe40b9284926518b1fc4bded4
HEZORAIME <chanrojano@gmail.com>
2025-08-05 23:00:50 +0800

    update(Userlimiter): set to 100 for development purposes only

--- Backend/src/middleware/productRatelimiter.js
+++ Backend/src/middleware/productRatelimiter.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const productRateLimiter = rateLimit({
     windowMs: 5 * 60 * 1000, // 5 minutes
-    max: 10, // max requests per 5 minutes
+    max: 100, // max requests per 5 minutes
     message: 'Too many requests, please try again later',
     // Skip rate limiting for admins
     skip: (req, _res) => {
