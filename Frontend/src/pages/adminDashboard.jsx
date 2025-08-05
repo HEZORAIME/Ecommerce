471b366da5b86578e86755d09e14780fdd1296c8
HEZORAIME <chanrojano@gmail.com>
2025-08-05 22:58:45 +0800

    style: fix code formatting and component naming

--- Frontend/src/pages/adminDashboard.jsx
+++ Frontend/src/pages/adminDashboard.jsx
@@ -10,5 +10,5 @@ export default function AdminDashboard() {
         <AdminProducts />
       </div>
     </div>
-  )
+  );
 }
--- Frontend/src/pages/userDashboard.jsx
+++ Frontend/src/pages/userDashboard.jsx
@@ -1,12 +1,12 @@
 import Navbar from '../components/Navbar'
 import UserProducts from '../components/UserProduct'
 
-export default function userDashboard() {
+export default function UserDashboard() {
   return (
     <div>
      <Navbar/>
-     <h1>User dashboard</h1>
+     <h1 className='text-3xl font-bold text-center my-8'>User dashboard</h1>
      <UserProducts/>
     </div>
-  )
+  );
 }
