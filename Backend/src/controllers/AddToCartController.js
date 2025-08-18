b3adad9275d962396effc9abf46eaa8b039c4d52
HEZORAIME <chanrojano@gmail.com>
2025-08-18 23:08:48 +0800

    wip(AddToCart): Work with it later!

--- Backend/src/controllers/AddToCartController.js
+++ Backend/src/controllers/AddToCartController.js
@@ -4,6 +4,12 @@ import mongoose from "mongoose";
 
 export const AddToCart = async (req, res) => {
     const productID = req.params.productID;
+    
 
-    if(mongoose.Types.ObjectId.isValid(productID))
+    if(!mongoose.Types.ObjectId.isValid(productID)) {
+        return res.status(400).json({message: "Invalid product ID"})
+    }
+    try {
+        const product = await Product.findById(productID);
+    }
 }
