951ccdae8d002db8b0a5489c343762c1da3baada
HEZORAIME <chanrojano@gmail.com>
2025-08-19 22:18:31 +0800

    refactor(userDashboard): remove redundant heading from dashboard

--- Frontend/src/pages/userDashboard.jsx
+++ Frontend/src/pages/userDashboard.jsx
@@ -5,7 +5,6 @@ export default function UserDashboard() {
   return (
     <div>
      <Navbar/>
-     <h1 className='text-3xl font-bold text-center my-8'>User dashboard</h1>
      <UserProducts/>
     </div>
   );
