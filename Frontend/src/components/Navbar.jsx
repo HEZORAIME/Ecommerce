8a24fd51de40eac8225639f2e489a3ecca77b7e5
HEZORAIME <chanrojano@gmail.com>
2025-08-25 21:16:12 +0800

    refactor(Navbar): improve layout structure and spacing

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -11,10 +11,24 @@ export default function Navbar() {
         }
     }
     return (
-        <div className='flex justify-end pr-2'>
-            <button className='btn btn-primary rounded-sm bg-red-400 text-lg' onClick={handleLogout}>
-                Logout
-            </button>
+<div className="flex justify-between items-center p-2">
+  <div className="w-1/1"></div>
+  
+  <div className="w-1/3 flex justify-center">
+        <div className="text-2xl font-bold text-white">
+            Dashboard
         </div>
+  </div>
+  
+  <div className="w-1/3 flex justify-end">
+    <button 
+      className="btn btn-primary rounded-sm bg-red-400 text-lg" 
+      onClick={handleLogout}
+    >
+      Logout
+    </button>
+  </div>
+</div>  
+
     )
 }
