47344f2e486ad67c47d2bde35ec4f3346351a3b0
HEZORAIME <chanrojano@gmail.com>
2025-08-30 21:49:48 +0800

    feat(EcomShop): create Shopna page and integrate UserProducts component

---
+++ Frontend/src/pages/EcomShop.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UserProducts from '../components/UserProduct'
+
+export default function Shopna() {
+    return (
+        <div>
+            <UserProducts />
+        </div>
+    )
+}
