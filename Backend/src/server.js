a30bb5ed8f24a0939757257935c2d2693dec6c78
HEZORAIME <chanrojano@gmail.com>
2025-10-30 12:20:47 +0800

    feat(server): add CartRoute to API routes

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import UserRoute from './routes/UserRoute.js'
 import cookieParser from 'cookie-parser';
 import ProductRoute from './routes/ProductRoute.js';
+import CartRoute from './routes/CartRoute.js';
 
 dotenv.config();
 
@@ -18,7 +19,7 @@ app.use(cookieParser());
 // api routes
 app.use("/api/users", UserRoute);
 app.use("/api/products", ProductRoute);
-
+app.use("/api/cart", CartRoute);
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("Server started on port", PORT);
